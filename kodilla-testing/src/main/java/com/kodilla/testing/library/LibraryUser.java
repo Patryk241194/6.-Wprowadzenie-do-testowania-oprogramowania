3692617d9c11c2fc856a55d72880f7e62c513aac
Patryk Potrykus <potrykus.patrykk@gmail.com>
2023-04-22 21:31:45 +0200

    Task 6.6: Expanding BookLibrary

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/LibraryUser.java
@@ -0,0 +1,58 @@
+package com.kodilla.testing.library;
+
+public class LibraryUser {
+
+    private String firstName;
+    private String lastName;
+    private String peselId;
+
+    public LibraryUser(String firstName, String lastName, String peselId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.peselId = peselId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPeselId() {
+        return peselId;
+    }
+
+    public void setPeselId(String peselId) {
+        this.peselId = peselId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LibraryUser that)) return false;
+
+        if (getFirstName() != null ? !getFirstName().equals(that.getFirstName()) : that.getFirstName() != null)
+            return false;
+        if (getLastName() != null ? !getLastName().equals(that.getLastName()) : that.getLastName() != null)
+            return false;
+        return getPeselId() != null ? getPeselId().equals(that.getPeselId()) : that.getPeselId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName() != null ? getFirstName().hashCode() : 0;
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getPeselId() != null ? getPeselId().hashCode() : 0);
+        return result;
+    }
+}
