ae3b17f311590a886178f13d44d34a7995396eab
Patryk Potrykus <potrykus.patrykk@gmail.com>
2023-05-05 21:38:52 +0200

    8.4. Exceptions and flight finder

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,46 @@
+package com.kodilla.exception.test;
+
+import com.kodilla.exception.nullpointer.MessageNotSentException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightRunner {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> mapOfAirports = new HashMap<>();
+        mapOfAirports.put("ATL", true);
+        mapOfAirports.put("PEK", true);
+        mapOfAirports.put("LAX", true);
+        mapOfAirports.put("DXB", true);
+        mapOfAirports.put("HND", true);
+        mapOfAirports.put("ORD", false);
+        mapOfAirports.put("PVG", false);
+        mapOfAirports.put("CDG", false);
+        mapOfAirports.put("DFW", false);
+        mapOfAirports.put("CAN", false);
+
+        String origin = flight.getDepartureAirport();
+        String destination = flight.getArrivalAirport();
+
+        if (mapOfAirports.get(origin) && mapOfAirports.get(destination) &&
+                !origin.equals(destination)) {
+            System.out.println("Departure from " + origin +
+                    " to " + destination + " is currently available");
+        } else {
+            throw new RouteNotFoundException("Departure from " + origin +
+                    " to " + destination + " is unavailable");
+        }
+    }
+
+    public static void main(String[] args) {
+        FlightRunner flightRunner = new FlightRunner();
+
+        try {
+            flightRunner.findFlight(new Flight("CDG", "PEK"));
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FlightRunnerTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.exception.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlightRunnerTestSuite {
+
+    @Test
+    public void testProbablyIWillThrowException() {
+        // given
+        FlightRunner flightRunner = new FlightRunner();
+
+        // when & then
+//        1) Airport with true to airport with true
+//        2) Departure and landing at the same airport
+//        3) Airport with true to airport with false
+//        4) Airport with false to airport with false
+        assertAll(
+                () -> assertDoesNotThrow(() -> flightRunner.findFlight(new Flight("ATL", "PEK"))),
+                () -> assertThrows(RouteNotFoundException.class, () -> flightRunner.findFlight(new Flight("LAX", "LAX"))),
+                () -> assertThrows(RouteNotFoundException.class, () -> flightRunner.findFlight(new Flight("ATL", "CAN"))),
+                () -> assertThrows(RouteNotFoundException.class, () -> flightRunner.findFlight(new Flight("CDG", "PEK")))
+        );
+    }
+}
