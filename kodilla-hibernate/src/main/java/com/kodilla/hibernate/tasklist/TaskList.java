899018ad73312f427749b28ef9c0e1ea455b7c70
Patryk Potrykus <potrykus.patrykk@gmail.com>
2023-07-10 21:37:32 +0200

    Submodule 17.3

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,7 +1,11 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -12,6 +16,8 @@ public class TaskList {
     private String listName;
 
     private String description;
+
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -29,6 +35,7 @@ public class TaskList {
         return id;
     }
 
+    @NotNull
     @Column(name = "LISTNAME")
     public String getListName() {
         return listName;
@@ -37,6 +44,16 @@ public class TaskList {
     @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
     }
 
     private void setId(int id) {
@@ -49,5 +66,9 @@ public class TaskList {
 
     private void setDescription(String description) {
         this.description = description;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
