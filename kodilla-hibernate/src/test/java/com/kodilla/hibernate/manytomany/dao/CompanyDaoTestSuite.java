dae8052bcc58ee8efc4eace1045a8071daa297be
Patryk241194 <128822106+Patryk241194@users.noreply.github.com>
2023-07-15 10:58:21 +0200

    17.4. Task: Named Queries (#5)
    * 17.4. Task: Named Queries
    
    * MinMaxValueFinder adjustments

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
@@ -13,6 +16,9 @@ public class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -60,4 +66,69 @@ public class CompanyDaoTestSuite {
         }
     }
 
+    @Test
+    void testNamedQueries() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        employeeDao.save(johnSmith);
+        int johnSmithId = johnSmith.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieClarcksonId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaKovalskyId = lindaKovalsky.getId();
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //When
+        List<Company> companiesStartingWithDAT = companyDao.findCompaniesByFirstThreeChars("Dat");
+        List<Company> companiesStartingWithSOF = companyDao.findCompaniesByFirstThreeChars("Sof");
+        List<Company> companiesStartingWithNUL = companyDao.findCompaniesByFirstThreeChars("Nul");
+        List<Employee> employeesWithSurnameSmith = employeeDao.findByLastName("Smith");
+        List<Employee> employeesWithSurnameClarckson = employeeDao.findByLastName("Clarckson");
+        List<Employee> employeesWithSurnameNovak = employeeDao.findByLastName("Novak");
+
+        //Then
+        assertEquals(1, companiesStartingWithDAT.size());
+        assertEquals(1, companiesStartingWithSOF.size());
+        assertEquals(0, companiesStartingWithNUL.size());
+        assertEquals(1, employeesWithSurnameSmith.size());
+        assertEquals(1, employeesWithSurnameClarckson.size());
+        assertEquals(0, employeesWithSurnameNovak.size());
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+            employeeDao.deleteById(johnSmithId);
+            employeeDao.deleteById(stephanieClarcksonId);
+            employeeDao.deleteById(lindaKovalskyId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/minmax/MinMaxValueFinder.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/minmax/MinMaxValueFinder.java
@@ -16,6 +16,32 @@ public class MinMaxValueFinder {
         return maxValue;
     }
 
+    public static int findSecondMaximumValue(int[] arrayOfNumbers) {
+        if (arrayOfNumbers.length < 2) {
+            throw new IllegalArgumentException("Array has less than 2 numbers");
+        }
+
+        int maxValue1 = arrayOfNumbers[0];
+        int maxValue2 = arrayOfNumbers[1];
+
+        if (maxValue2 > maxValue1) {
+            int tempValue = maxValue1;
+            maxValue1 = maxValue2;
+            maxValue2 = tempValue;
+        }
+
+        for (int i = 2; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] > maxValue1) {
+                maxValue2 = maxValue1;
+                maxValue1 = arrayOfNumbers[i];
+            } else if (arrayOfNumbers[i] > maxValue2 && arrayOfNumbers[i] != maxValue1) {
+                maxValue2 = arrayOfNumbers[i];
+            }
+        }
+
+        return maxValue2;
+    }
+
     public static int findMinimumValue(int[] arrayOfNumbers) {
         if (arrayOfNumbers.length == 0) {
             throw new IllegalArgumentException("Array is empty");
@@ -30,7 +56,31 @@ public class MinMaxValueFinder {
         return minValue;
     }
 
+    public static int findSecondMinimumValue(int[] arrayOfNumbers) {
+        if (arrayOfNumbers.length < 2) {
+            throw new IllegalArgumentException("Array has less than 2 numbers");
+        }
 
+        int minValue1 = arrayOfNumbers[0];
+        int minValue2 = arrayOfNumbers[1];
+
+        if (minValue2 < minValue1) {
+            int tempValue = minValue1;
+            minValue1 = minValue2;
+            minValue2 = tempValue;
+        }
+
+        for (int i = 2; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] < minValue1) {
+                minValue2 = minValue1;
+                minValue1 = arrayOfNumbers[i];
+            } else if (arrayOfNumbers[i] < minValue2 && arrayOfNumbers[i] != minValue1) {
+                minValue2 = arrayOfNumbers[i];
+            }
+        }
+
+        return minValue2;
+    }
 }
 
 
