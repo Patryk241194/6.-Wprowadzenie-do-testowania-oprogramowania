bc8608f48d60410e1f07157371c7f03f2d2a1b62
Patryk241194 <potrykus.patrykk@gmail.com>
2023-11-26 21:29:15 +0100

    Task submodule 30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -36,5 +36,31 @@ public class StoredProcTestSuite {
         statement2.close();
     }
 
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=false";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=true";
+
+        // When
+        Statement statement2 = dbManager.getConnection().createStatement();
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement2.executeUpdate(sqlProcedureCall);
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        // Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(1, howMany);
+        rs.close();
+        statement.close();
+        statement2.close();
+
+    }
 
 }
